eb5b740d05df9b7eeac0215666a4987dd5d085cf
povashraful <ashrafulhossainwork@gmail.com>
2026-07-27 19:38:12 +0200

    Updated the localization ROS 2 localization package-

---
+++ src/amr_localization/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'amr_localization'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ashraful',
+    maintainer_email='ashrafulhossainwork@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+
+            "particle_filter_node = amr_localization.particle_filter_node:main",
+        ],
+    },
+)
