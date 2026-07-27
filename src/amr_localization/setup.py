5b009410ffc4dd3fe48b3067c455746c4080bffe
povashraful <ashrafulhossainwork@gmail.com>
2026-07-27 22:36:27 +0200

    Updated and tested the localization package and filter node - {checked their communication and it seems everything is functioning properly

--- src/amr_localization/setup.py
+++ src/amr_localization/setup.py
@@ -1,31 +1,39 @@
 from setuptools import find_packages, setup
 
-package_name = 'amr_localization'
+
+package_name = "amr_localization"
+
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            "share/ament_index/resource_index/packages",
+            ["resource/" + package_name],
+        ),
+        (
+            "share/" + package_name,
+            ["package.xml"],
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ashraful',
-    maintainer_email='ashrafulhossainwork@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer="Ashraful",
+    maintainer_email="ashrafulhossainwork@gmail.com",
+    description=(
+        "Created a custom particle-filter localization package "
+        "for our AMR project."
+    ),
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-
-            "particle_filter_node = amr_localization.particle_filter_node:main",
+        "console_scripts": [
+            (
+                "particle_filter_node = "
+                "amr_localization.particle_filter_node:main"
+            ),
         ],
     },
 )
