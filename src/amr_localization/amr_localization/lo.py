c1b14544d1ea6a7be8259a14e571e7e285fde699
povashraful <ashrafulhossainwork@gmail.com>
2026-08-06 12:23:45 +0200

    Implemented particle motion prediction and RViz visualization

--- src/amr_localization/amr_localization/lo.py
+++ src/amr_localization/amr_localization/lo.py
@@ -7,6 +7,11 @@ from nav_msgs.msg import Odometry
 from rclpy.node import Node
 
 
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
+from geometry_msgs.msg import Quaternion
+
+
 @dataclass
 class particle:
     x: float
@@ -15,16 +20,26 @@ class particle:
     weight: float
 
 
-
-
 def quat_to_yaw_conversion(q):
     siny = 2 * (q.w * q.z + q.x * q.y)
 
     cosy = 1 - 2 * (q.y * q.y + q.z * q.z)
 
-
-
     return math.atan2(siny, cosy)
+
+
+def yaw_to_quaternion(yaw):
+
+    q = Quaternion()
+
+    q.x = 0.0
+    q.y = 0.0
+
+    q.z = math.sin(yaw / 2)
+
+    q.w = math.cos(yaw / 2)
+
+    return q
 
 
 class particle_filter(Node):
@@ -32,12 +47,28 @@ class particle_filter(Node):
     def __init__(self):
         super().__init__("particle_filter_node")
 
-        self.number_of_particles = 10 # can be tunes later
+        self.number_of_particles = 10  # can be tunes later
 
-        self.noise_position = 0.20 # can be modified later
-        self.noise_yaw = 0.15 # can be modified later
+        self.noise_position = 0.20  # can be modified later
+        self.noise_yaw = 0.15  # can be modified later
 
-        self.particles = self.gg_particles(initial_x=0.0, initial_y=0.0, initial_yaw=0.0)
+
+        self.particle_publisher = self.create_publisher(
+        
+                    PoseArray,
+                    "/particle_cloud",
+                    10,
+                )
+
+        
+        self.particles = self.gg_particles(
+            initial_x=0.0,
+            initial_y=0.0,
+            initial_yaw=0.0,
+        )
+
+        self.publish_particles()
+        
 
         self.previous_odometry_x = None
         self.previous_odometry_y = None
@@ -45,7 +76,14 @@ class particle_filter(Node):
 
         self.odometry_subscription = self.create_subscription(
 
-            Odometry,"/odom", self.odometry_callback_function,10)
+            Odometry,
+            "/odom",
+            self.odometry_callback_function,
+            10,
+        )
+
+       
+
 
         self.get_logger().info("Particle filter node started!")
 
@@ -61,10 +99,9 @@ class particle_filter(Node):
 
         for i in range(self.number_of_particles):
 
-            print(f"Creating particle {i}")
+            self.get_logger().info(f"Creating particle {i}")
 
             new_particle = particle(
-
 
                 x=random.gauss(initial_x, self.noise_position),
 
@@ -72,20 +109,61 @@ class particle_filter(Node):
 
                 yaw=random.gauss(initial_yaw, self.noise_yaw),
 
-                weight=weight_each
+                weight=weight_each,
 
-
-    )
+            )
 
             particle_list.append(new_particle)
 
         return particle_list
 
+    def motion_update(self, dx, dy, dyaw):
+
+        for p in self.particles:
+
+            noisy_dx = random.gauss(dx, self.noise_position)
+            noisy_dy = random.gauss(dy, self.noise_position)
+            noisy_dyaw = random.gauss(dyaw, self.noise_yaw)
+
+            p.x += noisy_dx
+            p.y += noisy_dy
+            p.yaw += noisy_dyaw
+
+        self.publish_particles()
+
+
+        
+
+    def publish_particles(self):
+
+        particle_cloud = PoseArray()
+
+        particle_cloud.header.frame_id = "map"
+        particle_cloud.header.stamp = self.get_clock().now().to_msg() 
+
+        for p in self.particles:
+
+            pose = Pose()
+
+            pose.position.x = p.x
+            pose.position.y = p.y
+            pose.position.z = 0.0
+
+            pose.orientation = yaw_to_quaternion(p.yaw)
+
+            particle_cloud.poses.append(pose)
+
+        self.particle_publisher.publish(particle_cloud)
+        self.get_logger().info("Publishing particle cloud")
+
+
     def odometry_callback_function(self, odometry_message):
 
         robot_x_position = odometry_message.pose.pose.position.x
         robot_y_position = odometry_message.pose.pose.position.y
-        robot_yaw_angle = quat_to_yaw_conversion(odometry_message.pose.pose.orientation)
+        robot_yaw_angle = quat_to_yaw_conversion(
+            odometry_message.pose.pose.orientation
+        )
 
         if self.previous_odometry_x is None:
 
@@ -97,23 +175,23 @@ class particle_filter(Node):
 
             return
 
-
         dx = robot_x_position - self.previous_odometry_x
         dy = robot_y_position - self.previous_odometry_y
         dyaw = robot_yaw_angle - self.previous_odometry_yaw
 
-        self.get_logger().info(f"dx={dx:.3f}, dy={dy:.3f}, dyaw={dyaw:.3f}")
 
 
+        
+        
+        self.motion_update(dx, dy, dyaw)
+
+        self.get_logger().info(
+            f"dx={dx:.3f}, dy={dy:.3f}, dyaw={dyaw:.3f}"
+        )
 
         self.previous_odometry_x = robot_x_position
         self.previous_odometry_y = robot_y_position
-
         self.previous_odometry_yaw = robot_yaw_angle
-
-
-
-
 
 
 def main(args=None):
